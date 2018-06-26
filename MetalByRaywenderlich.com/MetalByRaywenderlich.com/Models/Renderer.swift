ed6d123f3d4bf21a07c87728443e56ec99ab8b50
GEORGE QUENTIN <ggeoo93@gmail.com>
2018-06-26 23:25:27 +0100

    animating object and passing values to shader

--- MetalByRaywenderlich.com/MetalByRaywenderlich.com/Models/Renderer.swift
+++ MetalByRaywenderlich.com/MetalByRaywenderlich.com/Models/Renderer.swift
@@ -18,13 +18,34 @@ class Renderer: NSObject {
         -1,  1, 0,   // V0  top left
         -1, -1, 0,   // V1  bottom left
         1, -1, 0,   // V2   bottom right
-        1, -1, 0,   // V2   bottom right
+        //1, -1, 0,   // V2   bottom right
         1,  1, 0,   // V3   top right
-        -1,  1, 0    // V0  top left
+        //-1,  1, 0    // V0  top left
     ]
 
+    // the indices describe the two triangles and the order that we want to render the vertice
+    var indices: [UInt16] = [
+        0, 1, 2,
+        2, 3, 0
+    ]
+
+    // pipeline descriptor state for referencing shader
     var pipelineState: MTLRenderPipelineState?
+
+    // buffer for verices
     var vertexBuffer: MTLBuffer?
+
+    // buffer for indices
+    var indexBuffer: MTLBuffer?
+
+    // there will be a lot of occasion when you want to pass data to the GPU,
+    // you might want to send constant values to the GPU, to do this we can create a struct to hold
+    // the values that the GPU will then apply to all the vertices.
+    struct Constants {
+        var animateBy: Float = 0
+    }
+    var constants = Constants()
+    var time: Float = 0
 
     //MARK: - initialise the Renderer with a device
     init(device: MTLDevice) {
@@ -38,15 +59,18 @@ class Renderer: NSObject {
         super.init()
 
         //3) setup model and pipeline
-        buildModel()
+        buildModelVertices()
         buildPipelineState()
     }
 
-    // MARK: - create metal buffer that holds the vertices from the vertices array
-    private func buildModel() {
+    // MARK: - create metal buffer that holds the vertices and indices from the vertices and indices array
+    private func buildModelVertices() {
         vertexBuffer = device.makeBuffer(bytes: vertices,
                                          length: vertices.count * MemoryLayout<Float>.size,
                                          options: [])
+        indexBuffer = device.makeBuffer(bytes: indices,
+                                        length: indices.count * MemoryLayout<UInt16>.size,
+                                        options: [])
     }
 
     // MARK: - Setup pipeline state
@@ -93,7 +117,8 @@ extension Renderer: MTKViewDelegate {
         // you're only changing the list and not the original object.
         guard let drawable = view.currentDrawable,
             let descriptor = view.currentRenderPassDescriptor,
-            let pipelineState = self.pipelineState else { return }
+            let pipelineState = self.pipelineState,
+            let indexBuffer = self.indexBuffer else { return }
 
         //2) Create a command buffer to hold the command encoder
         let commandBuffer = commandQueue.makeCommandBuffer()!
@@ -113,14 +138,36 @@ extension Renderer: MTKViewDelegate {
         // you can see that if we had more complex buffers, we can tell the offset to start somewhere else in the buffer.
         // internaly, there is an index table of buffer and the at index specifies whihc entry in the table
         // the buffer is at. you can have up to 31 buffers.
+        // the vertex information is in the metal buffer at zero (0), so the GPU can tie the index information
+        // with the vertex information
         commandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
 
 
-        //6) finally we setup to draw, we specify that we are drawing a triangle primitive rather than a point or a line.
+
+        // 6)
+        // Instead of creating and sending a metal buffer as we did for the vertices and indices,
+        // we can directly send the address of the constant struct by using set vertex byte.
+        // as long as the data is less than 4000 bytes, then we do not have to create new metal buffers.
+        // metal can manage the buffer for us.
+        // As well as telling the GPU what the address of the struct is (Constant), we tell it how long
+        // the struct is using stride, and allocate a buffer index number just as we did when we created
+        // the metal vertex buffer.
+        time += 1 / Float(view.preferredFramesPerSecond)
+        let animateBy = abs(sin(time)/2 + 0.5) // sin values are between 1 and -1
+        constants.animateBy = animateBy
+        commandEncoder.setVertexBytes(&constants, length: MemoryLayout<Constants>.stride, index: 1)
+
+
+        //6a) finally we setup to draw, we specify that we are drawing a triangle primitive rather than a point or a line.
         // remember that we are setting a list of commands to the command encoder that get sent off in a batch
         // to the GPU, it is not until the commit at the end of the methods that we finish drawing or object.
-        commandEncoder.drawPrimitives(type: MTLPrimitiveType.triangle,
-                                      vertexStart: 0, vertexCount: vertices.count)
+        //commandEncoder.drawPrimitives(type: MTLPrimitiveType.triangle,
+        //                              vertexStart: 0, vertexCount: vertices.count)
+
+        //6b) we change the draw command to teel the GPU that we are now using the index order
+        commandEncoder.drawIndexedPrimitives(type: .triangle, indexCount: indices.count,
+                                             indexType: .uint16, indexBuffer: indexBuffer,
+                                             indexBufferOffset: 0)
 
         commandEncoder.endEncoding()
         commandBuffer.present(drawable)
--- MetalByRaywenderlich.com/MetalByRaywenderlich.com/Shader/Shader.metal
+++ MetalByRaywenderlich.com/MetalByRaywenderlich.com/Shader/Shader.metal
@@ -9,6 +9,11 @@
 #include <metal_stdlib>
 using namespace metal;
 
+struct Constants {
+    float animateBy;
+};
+
+
 // this is a vertex function so we prefix it with vertex and we are going to
 // return a float4 for the position of the vertex.
 // the parameters are the vertices array that you created in the Renderer,
@@ -17,14 +22,28 @@ using namespace metal;
 // the output of this function is the input of the next stage in the pipeline.
 // The GPU assembles the vertices into triagle primitives and the rasterizer
 // then takes over and splits our triangle into fragments.
+
+//vertex float4 vertex_shader(const device packed_float3 *vertices [[ buffer(0) ]],
+//                            uint vertexId [[ vertex_id ]]) {
+//    return float4(vertices[vertexId], 1);
+//}
+
+// we add the constants in the parameters to send data to the GPU, we give the constant the attribute
+// buffer 1 which is the buffer number that we allocated in the Renderer
 vertex float4 vertex_shader(const device packed_float3 *vertices [[ buffer(0) ]],
+                            constant Constants &constants [[ buffer(1) ]],
                             uint vertexId [[ vertex_id ]]) {
-    return float4(vertices[vertexId], 1);
+
+    float4 position = float4(vertices[vertexId], 1);
+    position.x += constants.animateBy;
+
+    return position;
 }
+
 
 // the fragment function the one that returns the color of each fragment is the one that is easier that the vertex fucntion.
 // it is a fragment function so we prefix it with fragment and returning a half4,
 // which is a smaller float4 and calling the function fragment_shader
 fragment half4 fragment_shader() {
-    return half4(1, 0, 0, 1);
+    return half4(1, 1, 0, 1);
 }
